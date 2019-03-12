e566cbbb42a45a3272e94603bc7f0c1bd75cbea8
Sebastian Pech <sebastian.pech@me.com>
2019-03-12 14:01:20 +0100

    Move edit function to misc

--- src/StringArrayEditor.jl
+++ src/StringArrayEditor.jl
@@ -70,47 +70,11 @@ function show(io::IO,f::File)
     print(io,"File(<Lines $(length(f.data))>,<Ref $(length(f.references))>)")
 end
 
-"""
-    edit(f::File)
-
-Save `f` to a temporary file and open with `edit`.
-"""
-function edit(f::File)
-    # Save file to tmp
-    path,io = mktemp()
-    for l in f.data
-        write(io,l,"\n")
-    end
-    close(io)
-    # Edit File
-    edit(path)
-    rm(path)
-end
-
 writeToFile(io::IOStream,l::String) = write(io,l)
 function writeToFile(io::IOStream,lines::Vector{String})
     for l in lines
         write(io,l,"\n")
     end
-end
-
-"""
-    edit(r<:Reference)
-
-Save `r` to a temporary file and open with `edit`.
-"""
-function edit(r::T) where T<:Reference
-    # Save file to tmp
-    path,io = mktemp()
-    writeToFile(io,value(r))
-    close(io)
-    # Edit File
-    edit(path)
-    # Reload edited stuff
-    _edit = readlines(path)
-    # Replace old with new
-    replace!(r,_edit)
-    rm(path)
 end
 
 function deletefromreferences!(f::File,del::Reference)
--- src/misc.jl
+++ src/misc.jl
@@ -6,3 +6,39 @@ function truncate(str::T,n::Int=30) where T<:AbstractString
 end
 
 @pre alive(self::T) where T<:Reference = isdestroyed(self) && error("Can't perform operation on deleted $T.")
+
+"""
+    edit(f::File)
+
+Save `f` to a temporary file and open with `edit`.
+"""
+function edit(f::File)
+    # Save file to tmp
+    path,io = mktemp()
+    for l in f.data
+        write(io,l,"\n")
+    end
+    close(io)
+    # Edit File
+    edit(path)
+    rm(path)
+end
+
+"""
+    edit(r<:Reference)
+
+Save `r` to a temporary file and open with `edit`.
+"""
+function edit(r::T) where T<:Reference
+    # Save file to tmp
+    path,io = mktemp()
+    writeToFile(io,value(r))
+    close(io)
+    # Edit File
+    edit(path)
+    # Reload edited stuff
+    _edit = readlines(path)
+    # Replace old with new
+    replace!(r,_edit)
+    rm(path)
+end
